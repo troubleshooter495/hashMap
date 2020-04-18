3793555aec6e99c6283bf727457b33888a2e6c81
troubleshooter495 <34400865+troubleshooter495@users.noreply.github.com>
2020-04-19 01:21:06 +0300

    fixed some code review comments

--- hash_map.h
+++ hash_map.h
@@ -11,24 +11,19 @@ public:
     using iterator = typename std::list<std::pair<const KeyType, ValueType>>::iterator;
     using const_iterator = typename std::list<std::pair<const KeyType, ValueType>>::const_iterator;
 
-    explicit HashMap(Hash hasher = Hash()) : hasher(hasher), iters(16), elements_num(0) {};
+    explicit HashMap(Hash hasher = Hash()) : hasher(hasher), iters(16) {};
 
     template<class IteratorType>
-    HashMap(IteratorType begin, IteratorType end, Hash hasher = Hash()) : hasher(hasher), iters(16), elements_num(0) {
+    HashMap(IteratorType begin, IteratorType end, Hash hasher = Hash()) : hasher(hasher), iters(16) {
         for (auto &&iter = begin; iter != end; iter++) {
             insert(*iter);
         }
     };
 
     HashMap(std::initializer_list<std::pair<KeyType, ValueType>> list, Hash hasher = Hash()) :
-            hasher(hasher), elements_num(0) {
-        iters.resize(default_size);
-        for (const auto &iter : list) {
-            insert(iter);
-        }
-    };
+            HashMap(list.begin(), list.end()) {};
 
-    size_t size() const {
+    uint64_t size() const {
         return elements_num;
     };
 
@@ -41,12 +36,12 @@ public:
     };
 
     iterator insert(std::pair<KeyType, ValueType> to_insert) {
-        const size_t hashed_key = hasher(to_insert.first);
+        const uint64_t hashed_key = hasher(to_insert.first);
         if (find(to_insert.first) != data.end()) {
             return find(to_insert.first);
         }
         if (iters[hashed_key % iters.size()].size() == bucket_size) {
-            rebuild();
+            expand();
         }
         data.push_back(to_insert);
         iters[hashed_key % iters.size()].push_back(std::prev(data.end(), 1));
@@ -55,7 +50,7 @@ public:
     };
 
     void erase(KeyType key) {
-        const size_t hashed_key = hasher(key);
+        const uint64_t hashed_key = hasher(key);
         auto possible_iter = iters[hashed_key % iters.size()].begin();
         for (const auto iter : iters[hashed_key % iters.size()]) {
             if (iter->first == key) {
@@ -85,7 +80,7 @@ public:
     }
 
     iterator find(KeyType key) noexcept {
-        const size_t hashed_key = hasher(key);
+        const uint64_t hashed_key = hasher(key);
         for (const auto &iter : iters[hashed_key % iters.size()]) {
             if (iter->first == key) {
                 return iter;
@@ -95,7 +90,7 @@ public:
     };
 
     const_iterator find(KeyType key) const noexcept {
-        const size_t hashed_key = hasher(key);
+        const uint64_t hashed_key = hasher(key);
         for (const auto &iter : iters[hashed_key % iters.size()]) {
             if (iter->first == key) {
                 return iter;
@@ -105,20 +100,16 @@ public:
     };
 
     ValueType &operator[](const KeyType key) {
-        if (find(key) == data.end()) {
-            insert({key, {}});
-        }
-        return find(key)->second;
+        auto found = insert({key, {}});
+        return found->second;
     };
 
     const ValueType &at(KeyType &&key) const {
         const auto &&found = find(key);
         if (found == data.end()) {
             throw std::out_of_range("out of range");
-        } else {
-            return found->second;
         }
-
+        return found->second;
     }
 
     void clear() {
@@ -143,12 +134,12 @@ private:
     Hash hasher;
     std::list<std::pair<const KeyType, ValueType>> data;
     std::vector<std::list<iterator>> iters;
-    const size_t default_size = 16;
-    const size_t bucket_size = 4;
-    u_int64_t elements_num;
+    static constexpr uint64_t default_size = 16;
+    static constexpr uint64_t bucket_size = 4;
+    uint64_t elements_num = 0;
 
-    void rebuild() {
-        const size_t new_size = iters.size() * 2;
+    void expand() {
+        const uint64_t new_size = iters.size() * 2;
         iters.clear();
         iters.resize(new_size);
         for (auto &&iter = data.begin(); iter != data.end(); ++iter) {
